4dbfe4dadc6877839ebf0b6a1fabda9b405c2fc9
Josh <vansjo01@luther.edu>
2020-11-12 10:54:32 -0600

    updated urls

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,6 @@ from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('backend.urls')),
     path('token-auth/', obtain_jwt_token),
     path('main/', include('main.urls')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import current_user, UserList, csrf, ping
+from .views import current_user, UserList, csrf, ping, index
 
 urlpatterns = [
     path('current_user/', current_user),
     path('users/', UserList.as_view()),
     path('csrf/', csrf),
     path('ping/', ping),
+    path('', index, name='index'),
 ]
