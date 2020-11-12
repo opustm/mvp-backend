5c73d771768ab8c8930ea9624c866e18ca6a219e
Josh <vansjo01@luther.edu>
2020-11-12 14:00:00 -0600

    Working with phone > charfield

--- main/models.py
+++ main/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import AbstractUser, Group
 class User(AbstractUser):
     picture = models.CharField(max_length=100, default='pic1')
     theme = models.CharField(max_length=100, default='theme1')
-    phone = models.BigIntegerField(default=9119119111)
-
+    phone = models.CharField(max_length=100, default='123-456-7890')
 
 class MyGroup(Group):
     picture = models.CharField(max_length=100, default='pic1')
