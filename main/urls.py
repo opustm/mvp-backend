070a01f07b67884f560b69fa8fa5371a075cb799
Josh <vansjo01@luther.edu>
2020-11-12 12:48:54 -0600

    updated views and url for backend project

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from .views import current_user, UserList, csrf, ping, index
+from .views import current_user, UserList, csrf, ping
 
 urlpatterns = [
     path('current_user/', current_user),
     path('users/', UserList.as_view()),
     path('csrf/', csrf),
     path('ping/', ping),
-    path('', index, name='index'),
 ]
