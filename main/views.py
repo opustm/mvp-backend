50523ea2590b23ffb0794421803d215fed47f01d
Josh <vansjo01@luther.edu>
2020-11-25 12:04:26 -0600

    Added team views with with GET by primary key

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import User
+from .models import User, Team
 from .serializers import TeamSerializer, UserSerializer, UserSerializerWithToken
 
 def index(request):
@@ -68,4 +68,38 @@ class UserDetail(APIView):
     def get(self, request, pk, format=None):
         user = self.get_object(pk)
         serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class TeamList(APIView):
+    """
+    Create a new user. It's called 'UserList' because normally we'd have a get
+    method here too, for retrieving a list of all User objects.
+    """
+
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = TeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, format=None):
+        allUsers = Team.objects.all()
+        serializer = TeamSerializer(allUsers, many=True)
+        return Response(serializer.data)
+
+class TeamDetail(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def get_object(self, pk):
+        try:
+            return Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            raise Http404("Team does not exist")
+
+    def get(self, request, pk, format=None):
+        team = self.get_object(pk)
+        serializer = TeamSerializer(team)
         return Response(serializer.data)
