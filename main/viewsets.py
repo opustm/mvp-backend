7a32ffd653cf18751a45f3f3252186b1e76ab44f
Joshua Van Sant <vansjo01@luther.edu>
2020-12-03 13:49:52 -0600

    attempting to change viewset permission

--- main/viewsets.py
+++ main/viewsets.py
@@ -1,23 +1,29 @@
 from rest_framework import viewsets
 from . import models
 from . import serializers
+from rest_framework import permissions, status
 
 class TeamViewset(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny,)
     queryset= models.Team.objects.all()
     serializer_class = serializers.TeamSerializer
     
 class UserViewset(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny,)
     queryset= models.User.objects.all()
     serializer_class = serializers.UserSerializer
 
 class InvitationViewset(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny,)
     queryset= models.Invitation.objects.all()
     serializer_class = serializers.InvitationSerializer
 
 class EventViewset(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny,)
     queryset= models.Event.objects.all()
     serializer_class = serializers.EventSerializer
 
 class ScheduleViewset(viewsets.ModelViewSet):
+    permission_classes = (permissions.AllowAny,)
     queryset= models.Schedule.objects.all()
     serializer_class = serializers.ScheduleSerializer
