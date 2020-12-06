bbaff656d7e7135add2de2cc768a82aecc08240c
Joshua Van Sant <vansjo01@luther.edu>
2020-12-05 18:47:04 -0600

    new token auth

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token
 from .views import documentation
 from .router import router
-from rest_framework.authtoken import views
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework.authtoken import obtain_auth_token
+
 
 
 urlpatterns = [
@@ -11,5 +12,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('token-auth/', views.obtain_auth_token)
+    path('token-auth/', obtain_auth_token)
 ]
