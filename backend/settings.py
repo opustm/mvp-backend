8384f12490273909fa067a434e53e174ea1968ac
Joshua Van Sant <vansjo01@luther.edu>
2020-12-05 21:36:56 -0600

    rm jwt from settings

--- backend/settings.py
+++ backend/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 'django_extensions',
     'rest_framework',
-    'rest_framework.auth_token',
-    'rest_framework_jwt',
+    # 'rest_framework.auth_token',
+    # 'rest_framework_jwt',
     'corsheaders',
     'main.apps.MainConfig',
 ]
